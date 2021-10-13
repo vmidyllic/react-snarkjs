a596b55384431e9a3e4803c8a8dcecacc1c1f2be
vmydillic <vladyslav@idyllicvision.com>
2021-10-13 15:53:01 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import React, { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import "./App.css";
 
-import { Field, Input, Text, Button, Link } from "rimble-ui";
-
+import { Alert, Button, Col, Container, Form, Row } from 'react-bootstrap';
 const snarkjs = require("snarkjs");
-
-const getSolidityProofArray = (proof: any) => {
-	let proofList = [
-		proof["pi_a"][0],
-		proof["pi_a"][1],
-		proof["pi_b"][0][1],
-		proof["pi_b"][0][0],
-		proof["pi_b"][1][1],
-		proof["pi_b"][1][0],
-		proof["pi_c"][0],
-		proof["pi_c"][1],
-	];
-	return proofList;
-};
 
 const makeProof = async (_proofInput: any, _wasm: string, _zkey: string) => {
 	const { proof, publicSignals } = await snarkjs.groth16.fullProve(_proofInput, _wasm, _zkey);
@@ -41,12 +28,12 @@ function App() {
 	const [signals, setSignals] = useState("");
 	const [isValid, setIsValid] = useState(false);
 
-	let wasmFile = "https://e306-95-158-35-18.ngrok.io/files/circuit.wasm";
-	let zkeyFile = "https://e306-95-158-35-18.ngrok.io/files/circuit_final.zkey";
-	let verificationKey = "https://e306-95-158-35-18.ngrok.io/files/verification_key.json";
+	let wasmFile = "https://a801-89-22-47-19.ngrok.io/files/circuit.wasm";
+	let zkeyFile = "https://a801-89-22-47-19.ngrok.io/files/circuit_final.zkey";
+	let verificationKey = "https://a801-89-22-47-19.ngrok.io/files/verification_key.json";
 
 	useEffect(() => {
-		window.addEventListener("rn-web-bridge", (event)=> {
+		window.addEventListener("rn-web-bridge", (event) => {
 			// @ts-ignore
 			alert(JSON.stringify(event.detail));
 		});
@@ -59,11 +46,14 @@ function App() {
 		let proofInput = { a, b };
 		console.log(proofInput);
 
-		window['ReactNativeWebView'].postMessage(JSON.stringify('JSON.stringify(window.location)'))
 		makeProof(proofInput, wasmFile, zkeyFile).then(({ proof: _proof, publicSignals: _signals }) => {
-			setProof(JSON.stringify(_proof, null, 2));
-			setSignals(JSON.stringify(_signals, null, 2));
-			window['ReactNativeWebView'].postMessage(JSON.stringify({_proof, _signals}))
+	
+			if (window['ReactNativeWebView']){
+				window['ReactNativeWebView'].postMessage(JSON.stringify({ _proof, _signals }))
+			}else{
+				setProof(JSON.stringify(_proof, null, 2));
+				setSignals(JSON.stringify(_signals, null, 2));
+			}
 			verifyProof(verificationKey, _signals, _proof).then((_isValid) => {
 				setIsValid(_isValid);
 			});
@@ -81,21 +71,44 @@ function App() {
 	return (
 		<div>
 			<header className="App-header">
-				<Text>
-					The underlying circuit is from the <a href="https://github.com/iden3/snarkjs">snarkjs readme</a>
-				</Text>
-				<pre>Witness Inputs</pre>
-				<Field label="Input a:">
-					<Input type="text" required={true} value={a} onChange={changeA} placeholder="e.g. 3" />
-				</Field>
-				<Field label="Input b:">
-					<Input type="text" required={true} value={b} onChange={changeB} placeholder="e.g. 11" />
-				</Field>
-				<Button.Outline onClick={runProofs}>Generate Proof</Button.Outline>
-				Proof: <Text width={1 / 2}>{proof}</Text>
-				Signals: <Text>{signals}</Text>
-				Result:
-				{proof.length > 0 && <Text>{isValid ? "Valid proof" : "Invalid proof"}</Text>}
+
+			<Alert  variant="primary">
+				This is a proof generator for circuit under the path <br />{wasmFile}
+  </Alert>
+				{/* <Form>
+					<Form.Group className="mb-3" controlId="formBasicEmail">
+						<Form.Label>Input a:</Form.Label>
+						<Form.Control type="number" placeholder="e.g. 3" />
+
+					</Form.Group>
+
+					<Form.Group className="mb-3" controlId="formBasicPassword">
+						<Form.Label>Input b:</Form.Label>
+						<Form.Control type="number" placeholder="e.g. 11" />
+					</Form.Group>
+				</Form> */}
+
+				<Button variant="outline-primary" onClick={runProofs}>Generate Proof</Button>
+				<Container   style={{ fontSize: 12 }}>
+					<Row>
+					<Col xs={3}></Col>
+					<Col xs={3}>Proof </Col>
+					<Col xs={6} >{proof}</Col>
+					</Row>
+					<Row>
+					<Col xs={3}></Col>
+
+					<Col xs={3}>Signals </Col>
+					<Col xs={6}>{signals}</Col>
+					</Row>
+					<Row>
+					<Col xs={3}></Col>
+
+					<Col xs={3}>Result </Col>
+					<Col xs={6}>{proof.length > 0 && <p>{isValid ? "Valid proof" : "Invalid proof"}</p>} </Col>
+					</Row>
+					</Container>
+				
 			</header>
 		</div>
 	);
