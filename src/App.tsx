0cef8b71dc3da23724783a86b49ba75a87f6283e
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2021-12-15 12:34:31 +0200

    add v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import "./App.css";
-
+import * as	 wc from  "./witness_calculator";
 import { Alert, Button, Col, Container, Form, Row } from 'react-bootstrap';
 const snarkjs = require("snarkjs");
+const fastFile =  require("fastfile");
+
+
 
 const makeProof = async (_proofInput: any, _wasm: string, _zkey: string) => {
 	console.log("make proof started");
+	
 	const { proof, publicSignals } = await snarkjs.groth16.fullProve(_proofInput, _wasm, _zkey);
+	return { proof, publicSignals };
+};
+
+const makeProofOnly = async ( _zkey: string, _wasm: any) => {
+	console.log("make proof v2 started");
+	const { proof, publicSignals } = await snarkjs.groth16.prove(_zkey,_wasm);
 	return { proof, publicSignals };
 };
 
@@ -29,9 +38,13 @@ function App() {
 	const [signals, setSignals] = useState("");
 	const [isValid, setIsValid] = useState(undefined);
 
-	let wasmFile = "/files/circuit.wasm";
-	let zkeyFile = "/files/circuit_final.zkey";
-	let verificationKey = "/files/verification_key.json";
+	let wasmFile = "/files/auth-v2/circuit.wasm";
+	let zkeyFile = "/files/auth-v2/circuit_final.zkey";
+	let verificationKey = "/files/auth-v2/verification_key.json";
+
+	/*
+
+	// auth-v1 version
 
 	window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
 		window['flutter_inappwebview'].callHandler('handlerProofRequest')
@@ -58,6 +71,34 @@ function App() {
 			});
 	});
 
+	*/
+
+
+	// auth v2 version
+	window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
+		window['flutter_inappwebview'].callHandler('handlerProofRequest')
+		  .then(function(inputs) {
+			// print to the console the data coming
+			// from the Flutter side.
+			console.log("webview recieved message from flutter:",JSON.stringify(inputs));
+			setProof(JSON.stringify("started v2 "));
+
+			fastFile.readExisting(wasmFile).then((fdWasm)=>{
+				 fdWasm.read(fdWasm.totalSize).then( wasm =>{
+					fdWasm.close();
+					wc.default(wasm).then(async witnessCalculator => {
+						witnessCalculator.calculateWTNSBin(inputs,0).then(buff =>{
+							makeProofOnly(zkeyFile,buff).then(({ proof: _proof, publicSignals: _signals }) =>{
+								window['flutter_inappwebview']
+								.callHandler('handlerProofResponse', { proof: _proof, publicSignals: _signals });
+						});
+					})
+				})
+				 })
+			})
+	});
+});
+
 	const runProofs = () => {
 
 			let proofInput = 
@@ -80,7 +121,8 @@ function App() {
 				"id": "360506537017543098982364518145035624387547643177965411252793105868750389248",
 				"state": "12051733342209181702880711377819237050140862582923079913097401558944144010618"
 			}
-			  
+
+		
 			  
 		if (window['ReactNativeWebView']){
 			alert('message is posted');
@@ -102,8 +144,67 @@ function App() {
 			});
 		}
 	};
+	const runProofsV2 = () => {
+	let proofInput = {
+			"BBJAx": "21293424947375383982389393614012508686131238764507830962167322403690693519627",
+			"BBJAy": "16598508602523319544157021146196633816724804268958683601720561163654916282271",
+			"BBJClaimClaimsTreeRoot": "20554237286409735067431599927785586172790530292725728679114269239512014723026",
+			"BBJClaimMtp": [
+			  "0",
+			  "0",
+			  "0",
+			  "0"
+			],
+			"BBJClaimRevTreeRoot": "0",
+			"BBJClaimRootsTreeRoot": "0",
+			"challenge": "12345",
+			"challengeSignatureR8x": "16860174036902244256104674167072183274610068074238677290884913730756513190577",
+			"challengeSignatureR8y": "326442332208888422237100058622311240286780945155390934232808034772471332948",
+			"challengeSignatureS": "1544566991663309777969185762263569084242157516252959158357266521174928119074",
+			"id": "288080072193943372832556802534124228514018653169991403332943485887956910080",
+			"state":"15683801318497632203100523443287756879812316361757309549093021254194216616235"
+	}
+		  
+		  
+	if (window['ReactNativeWebView']){
+		alert('message is posted');
+
+		window['ReactNativeWebView'].postMessage(JSON.stringify({proofInput}))
+	}else{
+		setProof(JSON.stringify("started"));
+
+		console.log(wasmFile);
+	
+		fastFile.readExisting(wasmFile).then((fdWasm)=>{
+			console.log("fdWasm",fdWasm)
+			 fdWasm.read(fdWasm.totalSize).then( wasm =>{
+				 console.log("wasm",wasm)
+				fdWasm.close();
+				wc.default(wasm).then(async witnessCalculator => {
+					console.log("witnessCalculator:", witnessCalculator)
+					witnessCalculator.calculateWTNSBin(proofInput,0).then(buff =>{
+						makeProofOnly(zkeyFile,buff).then(({ proof: _proof, publicSignals: _signals }) =>{
+							alert("make proof v2 finished");
+							setProof(JSON.stringify(_proof, null, 2));
+							setSignals(JSON.stringify(_signals, null, 2));
+						verifyProof(verificationKey, _signals, _proof).then((_isValid) => {
+							setIsValid(_isValid);});
+						})
+					});
+				})
+
+			 })
+		})
+
+		// 2. 
+
+
+		// 3.
+	}
+};
 
 	return (
+
 		<div>
 			<header className="App-header">
 
@@ -111,7 +212,7 @@ function App() {
 				This is a proof generator for circuit under the path <br />{wasmFile}
   </Alert>
 
-				<Button variant="outline-primary" onClick={runProofs}>Generate Proof</Button>
+				<Button variant="outline-primary" onClick={runProofsV2}>Generate Proof</Button>
 				<Container   style={{ fontSize: 12 }}>
 
 					
