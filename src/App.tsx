356b0beb99139b7f6b89300e73714f32707589a2
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-02-09 15:17:40 +0200

    support atomic query

--- src/App.tsx
+++ src/App.tsx
@@ -34,110 +34,79 @@ function App() {
 	const [a, setA] = useState("3");
 	const [b, setB] = useState("11");
 
-	const [proof, setProof] = useState("");
-	const [signals, setSignals] = useState("");
-	const [isValid, setIsValid] = useState(undefined);
-
-	let wasmFile = "/files/auth-v2/circuit.wasm";
-	let zkeyFile = "/files/auth-v2/circuit_final.zkey";
-	let verificationKey = "/files/auth-v2/verification_key.json";
+	const [proofAuth, setProofAuth] = useState("");
+	const [signalsAuth, setSignalsAuth] = useState("");
+	const [isValidAuth, setIsValidAuth] = useState(undefined);
 
 
-	// auth-v1 version
+	const [proofAtomic, setProofAtomic] = useState("");
+	const [signalsAtomic, setSignalsAtomic] = useState("");
+	const [isValidAtomic, setIsValidAtomic] = useState(undefined);
 
-	// window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
-	// 	console.log("flutterInAppWebViewPlatformReady is called!")
-	// 	console.log("setup handlerProofRequest for flutter_inappwebview object")
-	// 	window['flutter_inappwebview'].callHandler('handlerProofRequest')
-	// 	  .then(function(result) {
-	// 		// print to the console the data coming
-	// 		// from the Flutter side.
-	// 		console.log("webview recieved message from flutter:",JSON.stringify(result));
-	// 		setProof(JSON.stringify("started"));
-	// 		makeProof(result, wasmFile, zkeyFile).then(({ proof: _proof, publicSignals: _signals }) => {
-	// 		    console.log("make proof finished:");
-	// 			console.log({proof:_proof, signals:_signals});
-	// 			window['flutter_inappwebview']
-	// 		  .callHandler('handlerProofResponse', {proof:_proof, signals:_signals});
-	// 			});
-	// 		}).catch(err =>{
-	// 			console.log("error")
-	// 			console.log(err)
-	// 		});
-	// });
+	let wasmFilePathAuth = "/files/auth-v2/circuit.wasm";
+	let zkeyFilePathAuth = "files/auth-v2/circuit_final.zkey";
+	let verificationKeyFilePathAuth = "/files/auth-v2/verification_key.json";
 
 
+	let wasmFilePathAtomicQuery = "/files/credentialAtomicQuery/circuit.wasm";
+	let zkeyFilePathAtomicQuery = "/files/credentialAtomicQuery/circuit_final.zkey";
+	let verificationKeyPathAtomicQuery = "/files/credentialAtomicQuery/verification_key.json";
 	
+	type ProofGenerationInputs = {
+		circuitId :string;
+		inputs: any;
+	}
+	type ProofGenerationResult = {
+		circuitId :string;
+		proof: any;
+		pub_signals:any;
+	}
+
+async function processRequest (listOfInputs: ProofGenerationInputs[]): Promise<ProofGenerationResult[]> {
+	
+		debugger;
+		let proofGenerationResults :ProofGenerationResult[]= [];
+
+		for (const proofReq of listOfInputs){
+			if (proofReq.circuitId === "credentialAtomicQuery"){
+				let proofRes = await generateProof(wasmFilePathAtomicQuery,zkeyFilePathAtomicQuery,verificationKeyPathAtomicQuery,proofReq.inputs);
+				proofGenerationResults.push({circuitId: proofReq.circuitId, proof:proofRes.proof , pub_signals:proofRes.pub_signals});
+			}
+			if (proofReq.circuitId === "auth") {
+				let proofRes = await generateProof(wasmFilePathAuth,zkeyFilePathAuth,verificationKeyFilePathAuth,proofReq.inputs);
+				proofGenerationResults.push({circuitId: proofReq.circuitId, proof:proofRes.proof , pub_signals:proofRes.pub_signals});
+			}
+		}
+		return proofGenerationResults;
+	  }
+	async function generateProof(wasmFilePath: string,zkeyFilePath :string, verificationKeyPath:string,inputs:any) {
+		let fdWasm = await  fastFile.readExisting(wasmFilePath);
+		let wasm  = await fdWasm.read(fdWasm.totalSize);
+		fdWasm.close();
+		let witnessCalculator = await wc.default(wasm);
+		let wtnsBin = await witnessCalculator.calculateWTNSBin(inputs,0);
+		let { proof: _proof, pub_signals: _signals } = await makeProofOnly(zkeyFilePath,wtnsBin);
+
+		return  {proof:_proof, pub_signals:_signals};
+	}
 	// auth v2 version
 	window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
 		window['flutter_inappwebview'].callHandler('handlerProofRequest')
-		  .then(function(inputs) {
+		  .then(async function(listOfInputs: ProofGenerationInputs[]) {
 			// print to the console the data coming
 			// from the Flutter side.
-			console.log("webview recieved message from flutter:",JSON.stringify(inputs));
-			setProof(JSON.stringify("started v2 "));
+			console.log("webview recieved message from flutter (list expecting):",JSON.stringify(listOfInputs));
+			// parse inputs as array of proof generation inputs
 
-			fastFile.readExisting(wasmFile).then((fdWasm)=>{
-				 fdWasm.read(fdWasm.totalSize).then( wasm =>{
-					fdWasm.close();
-					wc.default(wasm).then(async witnessCalculator => {
-						witnessCalculator.calculateWTNSBin(inputs,0).then(buff =>{
-							makeProofOnly(zkeyFile,buff).then(({ proof: _proof, pub_signals: _signals }) =>{
-								window['flutter_inappwebview']
-								.callHandler('handlerProofResponse', { proof: _proof, pub_signals: _signals });
-						});
-					})
-				})
-				 })
-			})
+			
+			let proofGenResult = await processRequest(listOfInputs);
+
+			window['flutter_inappwebview']
+								.callHandler('handlerProofResponse', ...proofGenResult);
+			
 	});
 });
 
-	const runProofs = () => {
-
-			let proofInput = 
-			{
-				"BBJAx": "9582165609074695838007712438814613121302719752874385708394134542816240804696",
-				"BBJAy": "18271435592817415588213874506882839610978320325722319742324814767882756910515",
-				"BBJClaimClaimsTreeRoot": "4648350302718598839424502774166524253703556728225603109003078358379460427828",
-				"challenge": "12345",
-				"challengeSignatureR8x": "16612725446091862317820229560908560785741846639564668282377389669494210791554",
-				"challengeSignatureR8y": "16389928442818324712810234717861061407506363761548062772318825166082227839220",
-				"challengeSignatureS": "2734509058284749305743301031511411294322326439393517308790764830820315443120",
-				"BBJClaimMtp": [
-					"0",
-					"0",
-					"0",
-					"0"
-				],
-				"BBJClaimRevTreeRoot": "0",
-				"BBJClaimRootsTreeRoot": "0",
-				"id": "360506537017543098982364518145035624387547643177965411252793105868750389248",
-				"state": "12051733342209181702880711377819237050140862582923079913097401558944144010618"
-			}
-
-		
-			  
-		if (window['ReactNativeWebView']){
-			alert('message is posted');
-
-			window['ReactNativeWebView'].postMessage(JSON.stringify({proofInput}))
-		}else{
-			setProof(JSON.stringify("started"));
-
-			console.log(wasmFile);
-			makeProof(proofInput, wasmFile, zkeyFile).then(({ proof: _proof, pub_signals: _signals }) => {
-	
-				alert("make proof finished");
-
-					setProof(JSON.stringify(_proof, null, 2));
-					setSignals(JSON.stringify(_signals, null, 2));
-				verifyProof(verificationKey, _signals, _proof).then((_isValid) => {
-					setIsValid(_isValid);
-				});
-			});
-		}
-	};
 	const runProofsV2 = () => {
 	let proofInput = {
         "authClaim": [
@@ -247,6 +216,7 @@ function App() {
         "rootsTreeRoot": "0",
         "state": "15383795261052586569047113011994713909892315748410703061728793744343300034754"
     }
+	let proofInputAtomicQuery = {"authClaim":["164867201768971999401702181843803888060","0","17640206035128972995519606214765283372613874593503528180869261482403155458945","20634138280259599560273310290025659992320584624461316485434108770067472477956","15930428023331155902","0","0","0"],"authClaimMtp":["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"authClaimNonRevMtp":["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"authClaimNonRevMtpAuxHi":"0","authClaimNonRevMtpAuxHv":"0","authClaimNonRevMtpNoAux":"1","challenge":"1","challengeSignatureR8x":"8553678144208642175027223770335048072652078621216414881653012537434846327449","challengeSignatureR8y":"5507837342589329113352496188906367161790372084365285966741761856353367255709","challengeSignatureS":"2093461910575977345603199789919760192811763972089699387324401771367839603655","claim":["3677203805624134172815825715044445108615","293373448908678327289599234275657468666604586273320428510206058753616052224","10","0","30803922965249841627828060161","0","0","0"],"claimIssuanceClaimsTreeRoot":"7246896034587217404391735131819928831029447598354448731452631177424919458245","claimIssuanceIdenState":"3465800424177143196107127845857728750770736366457056414231195686681735039800","claimIssuanceMtp":["417537058197893761686953664555712220182002293231272771939654136223079364880","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"claimIssuanceRevTreeRoot":"0","claimIssuanceRootsTreeRoot":"0","claimNonRevIssuerClaimsTreeRoot":"7246896034587217404391735131819928831029447598354448731452631177424919458245","claimNonRevIssuerRevTreeRoot":"0","claimNonRevIssuerRootsTreeRoot":"0","claimNonRevIssuerState":"3465800424177143196107127845857728750770736366457056414231195686681735039800","claimNonRevMtp":["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"claimNonRevMtpAuxHi":"0","claimNonRevMtpAuxHv":"0","claimNonRevMtpNoAux":"1","claimSchema":"274380136414749538182079640726762994055","hoClaimsTreeRoot":"209113798174833776229979813091844404331713644587766182643501254985715193770","hoIdenState":"15383795261052586569047113011994713909892315748410703061728793744343300034754","hoRevTreeRoot":"0","hoRootsTreeRoot":"0","id":"293373448908678327289599234275657468666604586273320428510206058753616052224","operator":0,"slotIndex":2,"timestamp":"1642074362","value":"10"};
 		  
 		  
 	if (window['ReactNativeWebView']){
@@ -254,36 +224,37 @@ function App() {
 
 		window['ReactNativeWebView'].postMessage(JSON.stringify({proofInput}))
 	}else{
-		setProof(JSON.stringify("started"));
+		setProofAuth(JSON.stringify("started"));
+		setProofAtomic(JSON.stringify("started"));
 
-		console.log(wasmFile);
+		let listOfInputs: ProofGenerationInputs[] = [];
+		listOfInputs.push({circuitId:"auth",inputs:proofInput});
+		listOfInputs.push({circuitId:"credentialAtomicQuery",inputs:proofInputAtomicQuery});
+		
+		processRequest(listOfInputs).then(res=>{
+			console.log(res);
+			debugger;
+			alert("make proof array finished");
+
+			res.forEach(proofResult => {
+				
+				if (proofResult.circuitId === "auth"){
+					setProofAuth(JSON.stringify(proofResult.proof, null, 2));
+					setSignalsAuth(JSON.stringify(proofResult.pub_signals, null, 2));
+					verifyProof(verificationKeyFilePathAuth, proofResult.pub_signals, proofResult.proof).then((_isValid) => {
+					setIsValidAuth(_isValid);});
+				}
+				if (proofResult.circuitId === "credentialAtomicQuery"){
+					setProofAtomic(JSON.stringify(proofResult.proof, null, 2));
+					setSignalsAtomic(JSON.stringify(proofResult.pub_signals, null, 2));
+					verifyProof(verificationKeyPathAtomicQuery, proofResult.pub_signals, proofResult.proof).then((_isValid) => {
+					setIsValidAtomic(_isValid);});
+				}
 	
-		fastFile.readExisting(wasmFile).then((fdWasm)=>{
-			console.log("fdWasm",fdWasm)
-			 fdWasm.read(fdWasm.totalSize).then( wasm =>{
-				 console.log("wasm",wasm)
-				fdWasm.close();
-				wc.default(wasm).then(async witnessCalculator => {
-					console.log("witnessCalculator:", witnessCalculator)
-					witnessCalculator.calculateWTNSBin(proofInput,0).then(buff =>{
-						makeProofOnly(zkeyFile,buff).then(({ proof: _proof, pub_signals: _signals }) =>{
-							console.log(_signals);
-							alert("make proof v2 finished");
-							setProof(JSON.stringify(_proof, null, 2));
-							setSignals(JSON.stringify(_signals, null, 2));
-						verifyProof(verificationKey, _signals, _proof).then((_isValid) => {
-							setIsValid(_isValid);});
-						})
-					});
-				})
+			});
 
-			 })
 		})
-
-		// 2. 
-
-
-		// 3.
+	
 	}
 };
 
@@ -293,30 +264,49 @@ function App() {
 			<header className="App-header">
 
 			<Alert  variant="primary">
-				This is a proof generator for circuit under the path <br />{wasmFile}
+				This is a proof generator for circuit under the path <br />{wasmFilePathAuth}
+				<br />{wasmFilePathAtomicQuery}
   </Alert>
 
-				<Button variant="outline-primary" onClick={runProofsV2}>Generate Proof (new auth circuit v2)</Button>
+				<Button variant="outline-primary" onClick={runProofsV2}>Generate Proof (list support 2.0)</Button>
 				<Container   style={{ fontSize: 12 }}>
 
+					<Row>
+						<Col xs={5} >
+							<Row>
+								<Col xs={3}>Proof Auth </Col>
+								<Col xs={6} >{proofAuth}</Col>
+							</Row>
+							<Row>
+								<Col xs={3}>Signals Auth </Col>
+								<Col xs={6}>{signalsAuth}</Col>
+							</Row>
+							<Row>
+								<Col xs={3}>Result Auth </Col>
+								<Col xs={6}>{proofAuth.length > 0 && <p>{isValidAuth ? "Valid proof" : ( isValidAuth === undefined ? "calculating":  "Invalid proof")}</p>} </Col>
+							</Row>
+						</Col>
+						<Col xs={2}>
+							</Col>
+						<Col xs={5}>
+						<Row>
+								<Col xs={3}>Proof Atomic </Col>
+								<Col xs={6} >{proofAtomic}</Col>
+							</Row>
+							<Row>
+								<Col xs={3}>Signals Atomic </Col>
+								<Col xs={6}>{signalsAtomic}</Col>
+							</Row>
+							<Row>
+								<Col xs={3}>Result Atomic </Col>
+								<Col xs={6}>{proofAtomic.length > 0 && <p>{isValidAtomic ? "Valid proof" : ( isValidAtomic === undefined ? "calculating":  "Invalid proof")}</p>} </Col>
+							</Row>
+						</Col>
+
+					</Row>
+
 					
-					<Row>
-					<Col xs={3}></Col>
-					<Col xs={3}>Proof </Col>
-					<Col xs={6} >{proof}</Col>
-					</Row>
-					<Row>
-					<Col xs={3}></Col>
-
-					<Col xs={3}>Signals </Col>
-					<Col xs={6}>{signals}</Col>
-					</Row>
-					<Row>
-					<Col xs={3}></Col>
-
-					<Col xs={3}>Result </Col>
-					<Col xs={6}>{proof.length > 0 && <p>{isValid ? "Valid proof" : ( isValid === undefined ? "calculating":  "Invalid proof")}</p>} </Col>
-					</Row>
+				
 					</Container>
 				
 			</header>
